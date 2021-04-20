0bb9d2a5fbb4e2657d9d73fdd9c09fb4eb891add
Martin Maillard <self@martin-maillard.com>
2021-04-20 11:32:19 +0200

    feat: display full literals with datatype and language

--- src/components/ObservationValue.vue
+++ src/components/ObservationValue.vue
@@ -1,5 +1,8 @@
 <template>
-  <span>
+  <span v-if="isLiteral" :title="valueExpanded">
+    {{ value?.value }}
+  </span>
+  <span v-else class="tag bg-gray-200 whitespace-nowrap">
     {{ value?.value }}
   </span>
 </template>
@@ -8,6 +11,7 @@
 import { defineComponent } from 'vue'
 import { Term } from '@rdfjs/data-model'
 import { CubeDimension } from 'rdf-cube-view-query'
+import * as ns from '../namespace'
 
 export default defineComponent({
   name: 'ObservationValue',
@@ -19,6 +23,19 @@ export default defineComponent({
     dimension: {
       type: CubeDimension,
       required: true,
+    },
+  },
+
+  computed: {
+    isLiteral () {
+      return this.value && this.value.termType === 'Literal'
+    },
+
+    valueExpanded () {
+      const datatype = this.value.datatype ? `^^${ns.shrink(this.value.datatype.value)}` : ''
+      const language = this.value.language ? `@${this.value.language}` : ''
+
+      return `"${this.value.value}${language}"${datatype}`
     },
   },
 })
--- src/namespace.js
+++ src/namespace.js
@@ -1,10 +1,33 @@
 import namespace from '@rdfjs/namespace'
+import prefixes from '@zazuko/rdf-vocabularies/prefixes'
+import { shrink as _shrink } from '@zazuko/rdf-vocabularies/shrink'
+import { expand as _expand } from '@zazuko/rdf-vocabularies/expand'
 
 export const cube = namespace('https://cube.link/')
 export const meta = namespace('https://cube.link/meta/')
-export const qudt = namespace('http://qudt.org/schema/qudt/')
-export const rdf = namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-export const schema = namespace('http://schema.org/')
-export const sh = namespace('http://www.w3.org/ns/shacl#')
-export const time = namespace('http://www.w3.org/2006/time#')
+export const qudt = namespace(prefixes.qudt)
+export const rdf = namespace(prefixes.rdf)
+export const schema = namespace(prefixes.schema)
+export const sh = namespace(prefixes.sh)
+export const time = namespace(prefixes.time)
 export const view = namespace('https://cube.link/view/')
+
+export function expand (uri) {
+  if (uri && !uri.includes(':')) return uri
+
+  if (uri.startsWith('http://')) return uri
+
+  try {
+    return _expand(uri)
+  } catch {
+    return uri
+  }
+}
+
+export function shrink (uri, customBase) {
+  if (customBase && uri.startsWith(customBase)) {
+    return uri.replace(customBase, '').replace(/^[/#]/, '')
+  } else {
+    return _shrink(uri) || uri
+  }
+}
